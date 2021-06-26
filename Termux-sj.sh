12bc5fb696bdee597e99df8db71287f3b8b76a38
DOY-xpunten <85998921+DOY-xpunten@users.noreply.github.com>
2021-06-26 19:55:51 +0700

    Add files via upload

---
+++ Termux-sj.sh
@@ -0,0 +1,121 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[37;1m'
+sleep 1
+echo $green"[+]   LOADING   [+]"
+sleep 2
+echo $white"░░░░░░░░░░░░░▄▐░░░░"
+sleep 1
+echo "░░░░░░░▄▄▄░░▄██▄░░░"
+sleep 1
+echo "░░░░░░▐▀█▀▌░░░░▀█▄░"
+sleep 1
+echo "░░░░░░▐█▄█▌░░░░░░▀█▄"
+sleep 1
+echo "░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀"
+sleep 1
+echo "░░░░░▄▄▄██▀▀▀▀░░░░░"
+sleep 1
+echo "░░░░█▀▄▄▄█░▀▀░░░░░░"
+sleep 1
+echo "░░░░▌░▄▄▄▐▌▀▀▀░░░░░"
+sleep 1
+echo "░▄░▐░░░▄▄░█░▀▀░░░░░"
+sleep 1
+echo "░▀█▌░░░▄░▀█▀░▀░░░░░"
+sleep 1
+echo "░░░░░░░░▄▄▐▌▄▄░░░░░"
+sleep 1
+echo "░░░░░░░░▀███▀█▄░░░░"
+sleep 1
+echo "░░░░░░░▐▌▀▄▀▄▀▐░░░░"
+sleep 1
+echo "░░░░░░░▐▀░░░░░░▐▌░░"
+sleep 1
+echo "░░░░░░░█░░░░░░░░█░░"
+sleep 1
+echo "░░░░░░▐▌░░░░░░░░░█░"
+sleep 1
+echo
+echo $red"Jangan Lupa Follow Ig @doy_xpunten_"
+sleep 2
+echo $blue"Nah Tunggu Aja Lagi Paling 2 Detik Doang"
+sleep 2
+clear
+echo $green"Selamat Datang Kawan"
+sleep 1
+echo $red"Perkenalkan saya : "
+sleep 1
+echo
+toilet -f big -F gay Mr.DOY108/.
+sleep 2
+echo
+echo $red"Silahkan Pilih Tools Yang Disediakan Oleh Kami"
+sleep 1
+echo
+echo $cyan"1.) Stabilkan / root Jaringan "
+sleep 1
+echo
+echo $blue"0.) Keluar aja dech !!!"
+sleep 1
+echo
+echo $white
+read -p "Pilihlah Bree Jangan Lama  : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+sleep 2
+clear
+echo $green"[+] LOADING [+]"
+sleep 2
+echo $red"Tungguin Servernya Rusak :V"
+sleep 3
+echo $green"Canda Gw Gak Pake Server :V"
+sleep 2
+echo $white"Orang Gw Cuma Pake VPS :V"
+sleep 5
+clear
+sleep 2
+toilet -f big -F gay Mr.DOY108/.
+sleep 3
+echo $red"Saya Datang Kembali Untuk :"
+sleep 2
+echo $green" Stabilkan / Root Jaringan By Mr.DOY108/."
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+sleep 2
+echo $green"[+]   LOADING   [+]"
+sleep 3
+echo $white"▒▒▒▒▒▒▒▒▒▒▒▒"
+sleep 1
+echo "▒▒▒▒▓▒▒▓▒▒▒▒"
+sleep 1
+echo "▒▒▒▒▓▒▒▓▒▒▒▒"
+sleep 1
+echo "▒▒▒▒▒▒▒▒▒▒▒▒"
+sleep 1
+echo "▒▓▒▒▒▒▒▒▒▒▓▒"
+sleep 1
+echo "▒▒▓▓▓▓▓▓▓▓▒▒"
+sleep 1
+echo "▒▒▒▒▒▒▒▒▒▒▒▒"
+sleep 1
+echo $blue"Jangan Lupa Follow Instagram @doy_xpunten_"
+sleep 2
+echo $red"terima kasih atas kunjungannya"
+sleep 3
+echo $white"To Be Continued"
+sleep 4
+exit
+fi
+
